92d645da3fb918c85c18262403b081f67ee8543b
haidaraib <nezarmaarof@gmail.com>
2025-08-09 01:23:25 +0300

    bug fixes

--- HomeServices/to_deploy/core/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import User, NormalUser, Balance
-
-admin.site.register(User)
-admin.site.register(NormalUser)
-admin.site.register(Balance)
