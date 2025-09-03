74ae6372a5fb4971ed03b852da9a1ecc4b02554a
haidaraib <nezarmaarof@gmail.com>
2025-09-03 13:49:49 +0300

    bug fixes

--- HomeServices/services/apps.py
+++ HomeServices/services/apps.py
@@ -9,6 +9,7 @@ class ServicesConfig(AppConfig):
     def ready(self):
         # Connect signal after migrations
         post_migrate.connect(self.init_syrian_governorates, sender=self)
+        post_migrate.connect(self.init_service_categories, sender=self)
     
     def init_syrian_governorates(self, **kwargs):
         """Create Syrian governorates if they don't exist"""
@@ -36,6 +37,30 @@ class ServicesConfig(AppConfig):
         try:
             for gov in syrian_governorates:
                 Area.objects.get_or_create(name=gov)
+        except:
+            # Silently fail if table doesn't exist yet
+            pass
+    
+    def init_service_categories(self, **kwargs):
+        """Create default service categories if they don't exist"""
+        from .models import Category
+
+        default_categories = [
+            "تسليك",
+            "تمديدات كهرباء",
+            "نقل أثاث",
+            "دهان",
+            "سباكة",
+            "نجارة",
+            "تنظيف",
+            "صيانة أجهزة كهربائية",
+            "تركيب سيراميك",
+            "حدادة"
+        ]
+
+        try:
+            for cat in default_categories:
+                Category.objects.get_or_create(name=cat)
         except:
             # Silently fail if table doesn't exist yet
             pass
